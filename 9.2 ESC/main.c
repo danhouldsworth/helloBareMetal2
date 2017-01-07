fc6e250f7e3583a1545d8623f291bb707a4ee30c
Dan Houldsworth <dan@kew.io>
2017-01-07 09:27:30 +0000

    tidy format

--- 9.2 ESC/main.c
+++ 9.2 ESC/main.c
@@ -1,12 +1,26 @@
 #include <avr/interrupt.h>
 
+// --- PORT B -----
+#define	BpFET 2
+#define	CpFET 1
+#define BpFET_on 	PORTB &= ~(1 << BpFET)
+#define BpFET_off	PORTB |=  (1 << BpFET)
+#define CpFET_on 	PORTB &= ~(1 << CpFET)
+#define CpFET_off	PORTB |=  (1 << CpFET)
+// ----------------
+
+// --- PORT C -----
+#define RED_ON 		PORTC &= ~(1 << PORTC3)
+#define RED_OFF		PORTC |=  (1 << PORTC3)
+#define GRN_ON 		PORTC &= ~(1 << PORTC2)
+#define GRN_OFF		PORTC |=  (1 << PORTC2)
+// ----------------
+
+// --- PORT D -----
 #define	CnFET 5
 #define	BnFET 4
 #define	AnFET 3
 #define	ApFET 2
-#define	BpFET 2
-#define	CpFET 1
-
 #define AnFET_off 	PORTD &= ~(1 << AnFET)
 #define AnFET_on	PORTD |=  (1 << AnFET)
 #define BnFET_off 	PORTD &= ~(1 << BnFET)
@@ -15,20 +29,25 @@
 #define CnFET_on	PORTD |=  (1 << CnFET)
 #define ApFET_on 	PORTD &= ~(1 << ApFET)
 #define ApFET_off	PORTD |=  (1 << ApFET)
-#define BpFET_on 	PORTB &= ~(1 << BpFET)
-#define BpFET_off	PORTB |=  (1 << BpFET)
-#define CpFET_on 	PORTB &= ~(1 << CpFET)
-#define CpFET_off	PORTB |=  (1 << CpFET)
+// ----------------
 
-#define RED_ON 		PORTC &= ~(1 << PORTC3)
-#define RED_OFF		PORTC |=  (1 << PORTC3)
-#define GRN_ON 		PORTC &= ~(1 << PORTC2)
-#define GRN_OFF		PORTC |=  (1 << PORTC2)
+uint16_t startTime 	= 0;
+uint16_t pulseWidth 	= 0;
 
-uint16_t startTime = 0;
-uint16_t pulseWidth = 0;
+uint8_t waitingForT0 	= 0;
+uint8_t T2hb 		= 2;
 
-uint8_t PWM_nFET = 'O';
+uint8_t PWM_nFET 	= '';
+
+ISR(TIMER0_OVF_vect){
+	if (waitingForT0) waitingForT0--;
+}
+
+void wait16ms(uint8_t count){
+	waitingForT0 = count;
+	TCNT0 = 0;
+	while (waitingForT0){}
+}
 
 ISR(TIMER1_CAPT_vect){
 	uint16_t timer = ICR1;
@@ -50,14 +69,22 @@ ISR(TIMER2_COMP_vect){
 		default : AnFET_off;BnFET_off;CnFET_off;
 	}
 }
-
 ISR(TIMER2_OVF_vect){
+	if (T2hb++ < 2) {return;}
+	T2hb = 2;
+
 	switch (PWM_nFET){
 		case 'A' : AnFET_on; break;
 		case 'B' : BnFET_on; break;
 		case 'C' : CnFET_on; break;
 		default : AnFET_off;BnFET_off;CnFET_off;
 	}
+}
+void wait128us(){
+	waitingForT0 = 1;
+	TCNT0 = 0;
+	while(waitingForT0){}
+	return;
 }
 void waitForEdge(uint8_t edgeCompType){
 	uint8_t targetEdge = edgeCompType << ACO;
@@ -95,10 +122,10 @@ ACME 		ADEN 		MUX2..0 	Analog Comparator Negative Input
 */
 
 int main(void){
-	TCCR0 	= (1<<CS02) | (1<<CS01) | (0<<CS00); 				// Commutations	 2MHz
-	TCCR1B 	= (0<<CS12) | (0<<CS11) | (1<<CS10) | (1<<ICNC1); 	// RC INPUT 	16MHz[h/w noise reduction on Input Capture]
+	TCCR0 	= (0<<CS02) | (1<<CS01) | (0<<CS00); 				// Manual Commutations	 2MHz
+	TCCR1B 	= (0<<CS12) | (0<<CS11) | (1<<CS10) | (1<<ICNC1); 		// RC INPUT 	16MHz[h/w noise reduction on Input Capture]
 	TCCR2 	= (0<<CS22) | (1<<CS21) | (0<<CS20); 				// FET PWM 		 2MHz
-	TIMSK 	= (0<<TOIE0)| (0<<TOIE1) | (1<<TOIE2) | (1<<TICIE1) | (1<<OCIE2);
+	TIMSK 	= (1<<TOIE0)| (0<<TOIE1) | (1<<TOIE2) | (1<<TICIE1) | (1<<OCIE2);
 	TIFR 	= (1<<TOIE0)| (1<<TOIE1) | (1<<TOIE2) | (1<<TICIE1) | (1<<OCIE2);
 
 	OCR2 	= 0;
@@ -118,7 +145,6 @@ int main(void){
 	sei();
 	RED_ON;
 	while(1){
-
 		if (pulseWidth < 16000 || pulseWidth > 32000) {
 			AnFET_off;BnFET_off;CnFET_off;
 			ApFET_off;BpFET_off;CpFET_off;
@@ -126,9 +152,10 @@ int main(void){
 			GRN_OFF;
 		} else {
 			GRN_ON;
-			OCR2 = (pulseWidth - 16000) >> 7;
+			OCR2 = (pulseWidth - 16000) >> 8;
 			if (OCR2 < 1) 	OCR2 = 1;
-			if (OCR2 > 50) 	OCR2 = 50;
+			// if (OCR2 > 10) 	OCR2 = 10;
+			// OCR2 = 1;
 			TCNT2 = 0;
 			AnFET_off;BnFET_off;CnFET_off;
 			ApFET_off;BpFET_off;CpFET_off;
@@ -163,11 +190,11 @@ Estimations on crude timing
 	--> ~3.3ms per loop
 	--> ~550us per commutation section
 
-CS12 	CS11 	CS10	Description 	RATE 	Tick 	 Ticks/us 	8-bitOVF 	16-bitOVF
+CS12 		CS11 		CS10		Description 		RATE 	Tick 	    Ticks/us 	8-bitOVF 	16-bitOVF
 0 		0 		0		No clock
-0 		0 		1		clk/1 			 16MHz   60ns  		16		  16us	      4096us
-0 		1 		0		clk/8 			  2MHz  0.5us 		 2		 128us  	    33ms
-0 		1 		1		clk/64 		 	250KHz 	  4us 		1/4		 1.0ms 		   250ms
-1 		0 		0		clk/256  		 62KHz 	 16us 		1/16	 4.1ms 		   1.0s
-1 		0 		1		clk/1024 		 15KH`   64us 		1/64	16.4ms 		   4.2s
+0 		0 		1		clk/1 			 16MHz   60ns  		 16 	  16us	    	4096us
+0 		1 		0		clk/8 			  2MHz  0.5us 		  2 	 128us  	  33ms
+0 		1 		1		clk/64 		 	250KHz 	  4us 		1/4 	 1.0ms 		 250ms
+1 		0 		0		clk/256  		 62KHz 	 16us 		1/16	 4.1ms 		 1.0s
+1 		0 		1		clk/1024 		 15KH`   64us 		1/64	16.4ms 		 4.2s
 */
